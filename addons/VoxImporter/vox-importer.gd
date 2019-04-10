8ef3abd72f092a410324963c7a7dad2f48365f3c
Nicholas Rodine <halfofastaple@gmail.com>
2019-04-09 20:15:47 -0400

    Extracted chunk loading code into its own function, and created classes to hold more complex data.

--- addons/VoxImporter/vox-importer.gd
+++ addons/VoxImporter/vox-importer.gd
@@ -54,69 +54,34 @@ func import(source_path, destination_path, options, _platforms, _gen_files):
 	var identifier = PoolByteArray([ file.get_8(), file.get_8(), file.get_8(), file.get_8() ]).get_string_from_ascii()
 	#warning-ignore:unused_variable
 	var version = file.get_32()
-	var voxels = {}
-	var colors = null
-	var sizeX = 0
-	var sizeY = 0
-	var sizeZ = 0
-
+	
+	var vox = VoxNode.new();
 	if identifier == 'VOX ':
 		while file.get_position() < file.get_len():
-			var chunkId = PoolByteArray([ file.get_8(), file.get_8(), file.get_8(), file.get_8() ]).get_string_from_ascii()
-			var chunkSize = file.get_32()
-			#warning-ignore:unused_variable
-			var childChunks = file.get_32()
-
-			match chunkId:
-				'SIZE':
-					sizeX = file.get_32()
-					sizeY = file.get_32()
-					sizeZ = file.get_32()
-					print('size: ', sizeX, ', ', sizeY, ', ', sizeZ)
-					file.get_buffer(chunkSize - 4 * 3)
-				'XYZI':
-					#warning-ignore:unused_variable
-					for i in range(file.get_32()):
-						var x = file.get_8()
-						var z = -file.get_8()
-						var y = file.get_8()
-						var c = file.get_8()
-						var voxel = Vector3(x, y, z)
-						voxels[voxel] = c - 1
-				'RGBA':
-					colors = []
-					#warning-ignore:unused_variable
-					for i in range(256):
-						var r = float(file.get_8() / 255.0)
-						var g = float(file.get_8() / 255.0)
-						var b = float(file.get_8() / 255.0)
-						var a = float(file.get_8() / 255.0)
-						colors.append(Color(r, g, b, a))
-				_:
-						file.get_buffer(chunkSize)
-		
-		if voxels.size() == 0: return voxels
+			read_chunk(vox, file);
 	file.close()
 
-	var diffVector = Vector3(sizeX / 2 - 0.5, -0.5, -sizeY / 2 - 0.5)
-	print('diffVector: ', diffVector)
-
 	var st = SurfaceTool.new()
-	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-
-	for voxel in voxels:
-		var voxelSides = []
-		if not voxels.has(Vector3(voxel.x, voxel.y + 1, voxel.z)): voxelSides += top
-		if not voxels.has(Vector3(voxel.x, voxel.y - 1, voxel.z)): voxelSides += bottom
-		if not voxels.has(Vector3(voxel.x - 1, voxel.y, voxel.z)): voxelSides += left
-		if not voxels.has(Vector3(voxel.x + 1, voxel.y, voxel.z)): voxelSides += right
-		if not voxels.has(Vector3(voxel.x, voxel.y, voxel.z + 1)): voxelSides += front
-		if not voxels.has(Vector3(voxel.x, voxel.y, voxel.z - 1)): voxelSides += back
-		
-		st.add_color(colors[voxels[voxel]])
-
-		for t in voxelSides:
-			st.add_vertex(((t * 0.5) + voxel - diffVector) * scale)
+	
+	for model in vox.models:
+		var diffVector = model.size / 2 - Vector3(0.5, 0.5, 0.5)
+		print('diffVector: ', diffVector)
+	
+		st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
+		for voxel in model.voxels:
+			var voxelSides = []
+			if not model.voxels.has(Vector3(voxel.x, voxel.y + 1, voxel.z)): voxelSides += top
+			if not model.voxels.has(Vector3(voxel.x, voxel.y - 1, voxel.z)): voxelSides += bottom
+			if not model.voxels.has(Vector3(voxel.x - 1, voxel.y, voxel.z)): voxelSides += left
+			if not model.voxels.has(Vector3(voxel.x + 1, voxel.y, voxel.z)): voxelSides += right
+			if not model.voxels.has(Vector3(voxel.x, voxel.y, voxel.z + 1)): voxelSides += front
+			if not model.voxels.has(Vector3(voxel.x, voxel.y, voxel.z - 1)): voxelSides += back
+			
+			st.add_color(vox.colors[model.voxels[voxel]])
+	
+			for t in voxelSides:
+				st.add_vertex(((t * 0.5) + voxel - diffVector) * scale)
 		
 	st.generate_normals()
 	
@@ -130,6 +95,64 @@ func import(source_path, destination_path, options, _platforms, _gen_files):
 	
 	var full_path = "%s.%s" % [ destination_path, get_save_extension() ]
 	return ResourceSaver.save(full_path, mesh)
+
+class VoxNode:
+	var models: Array = [Model.new()];
+	var current_index = -1;
+	var colors = null;
+	
+	func get_model() -> Model: return models[current_index];
+
+class Model:
+	#warning-ignore:unused_class_variable
+	var size: Vector3;
+	#warning-ignore:unused_class_variable
+	var voxels = {};
+
+func read_chunk(vox: VoxNode, file: File):
+	var chunkId = PoolByteArray([ file.get_8(), file.get_8(), file.get_8(), file.get_8() ]).get_string_from_ascii();
+	var chunkSize = file.get_32();
+	#warning-ignore:unused_variable
+	var childChunks = file.get_32()
+
+	match chunkId:
+		'SIZE':
+			vox.current_index += 1;
+			var model = vox.get_model();
+			var x = file.get_32();
+			var z = file.get_32();
+			var y = file.get_32();
+			model.size = Vector3(x, y, z);
+			print('SIZE: ', x, ', ', y, ', ', z)
+			#warning-ignore:return_value_discarded
+			file.get_buffer(chunkSize - 4 * 3)
+		'XYZI':
+			var model = vox.get_model();
+			print('XYZI')
+			for _i in range(file.get_32()):
+				var x = file.get_8()
+				var z = model.size.z - file.get_8()
+				var y = file.get_8()
+				var c = file.get_8()
+				var voxel = Vector3(x, y, z)
+				model.voxels[voxel] = c - 1
+		'RGBA':
+			print('RGBA');
+			vox.colors = []
+			for _i in range(256):
+				var r = float(file.get_8() / 255.0)
+				var g = float(file.get_8() / 255.0)
+				var b = float(file.get_8() / 255.0)
+				var a = float(file.get_8() / 255.0)
+				vox.colors.append(Color(r, g, b, a))
+		'MAIN':
+			print('MAIN')
+			#warning-ignore:return_value_discarded
+			file.get_buffer(chunkSize)
+		_:
+			print(chunkId, ' - UNHANDLED');
+			#warning-ignore:return_value_discarded
+			file.get_buffer(chunkSize)
 
 var top = [
 	Vector3( 1.0000, 1.0000, 1.0000),
