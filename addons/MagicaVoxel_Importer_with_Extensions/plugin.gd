4e2fec2817534a2a4f63cab74962610512193ed0
Jonathan Heard <jon.heard.tutor@gmail.com>
2022-12-24 11:43:01 -0500

    - refactor - Conformed to Godot's style standards.

--- addons/MagicaVoxel_Importer_with_Extensions/plugin.gd
+++ addons/MagicaVoxel_Importer_with_Extensions/plugin.gd
@@ -9,7 +9,7 @@ func _enter_tree():
 	pluginToMeshLibrary = preload('vox-importer-meshLibrary.gd').new()
 	add_import_plugin(pluginToMesh)
 	add_import_plugin(pluginToMeshLibrary)
-	add_custom_type("FramedMeshInstance", "MeshInstance", preload("FramedMeshInstance.gd"), preload("FramedMeshInstance.png"))
+	add_custom_type("FramedMeshInstance", "MeshInstance", preload("framed_mesh_instance.gd"), preload("framed_mesh_instance.png"))
 
 func _exit_tree():
 	remove_import_plugin(pluginToMesh)
